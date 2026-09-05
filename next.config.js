8bd5e0e373306669a6caee08e529c8d2614c4464
dev-chinwan <pradeepchinwan.dev@gmail.com>
2026-09-05 22:32:35 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Explicit Turbopack section avoids Next 16 build failure when webpack config is also present.
+  turbopack: {},
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
